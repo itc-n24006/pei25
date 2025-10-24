64bf013e0b0ba3e472a5aa4686eaedfce32e8d24
itc-n24006 <n24006@std.it-college.ac.jp>
2025-10-24 03:41:39 +0000

    1024kadai

---
+++ m1_q5_27kadai.py
@@ -0,0 +1,4 @@
+list_d = ['マンゴー','パイナップル','バンシルー']
+a = list_d.pop(1)
+print(a)# 好きだから
+print(list_d)  
---
+++ m1_q5_28kadai.py
@@ -0,0 +1,9 @@
+day = ['月','月','火','水','木','金','金']
+
+day.remove('月')
+day.remove('金')
+
+day.insert(5, "土")
+day.insert(6, "日")
+
+print(day)
